94b255124f9832204e864312ebf9dca8c9eda892
Anirudh Sanjeev <a@ninjagod.com>
2011-02-21 16:03:27 +0530

    euler9

---
+++ euler9.scala
@@ -0,0 +1,20 @@
+object Euler9{
+    def main(args:Array[String])  {
+        // Wrong solution. I misread the question.
+
+
+
+        // Naive brute-force solution
+        // The maximum number can be 32
+        //val numSquared = (1 to 32).map(x => x*x);
+        //numSquared.foreach(a => numSquared.filter(_ > a).foreach(b => numSquared.filter(_ > b).find(c => {
+            //if((a + b + c)==1000){
+                //println (math.sqrt(a * b * c))
+                //true;
+            //}
+            //false;
+            //})));
+    }
+}
+
+// vim: set ts=4 sw=4 et:
