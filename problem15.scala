4845609e0c7c4d3df2ecb6f6cab3cd64f433f37d
Anirudh Sanjeev <a@ninjagod.com>
2011-02-24 10:37:12 +0530

    problem 15

---
+++ problem15.scala
@@ -0,0 +1,15 @@
+object Euler15  {
+    def main(args:Array[String])  {
+        // Since we only turn Right, and Down - we need a 40 character string
+        // which is RRDRR or RDRRD, etc. With 20 Rs, and 20 Ds.
+        //
+        // In other terms - redefine the problem as having 20Rs and 20Ds and
+        // finding the number of ways to place them. 40! is the number of permutations
+        // if Rs and Ds are not identical, but they are, so the answer is
+        //
+        // 40! / (20! * 20!)
+        
+    }
+}
+
+// vim: set ts=4 sw=4 et:
